ecdcb6317f73cad05d67a746eedff6f5be8f8ecf
hsienwei <hsienwei@gmail.com>
2022-05-05 21:18:39 +0800

    [What] Extract MainGame component

---
+++ src/components/mineswapper/MainGame.tsx
@@ -0,0 +1,75 @@
+import { BlockState, Game, GridState, ILevel } from "../../api/Game";
+
+
+
+
+function getAppearClass(state: BlockState) {
+    switch (state.state) {
+        case GridState.BLOCKED:
+            return "Block";
+        case GridState.OPENED:
+            return `Open Count${state.mineAroundCount}`;
+        case GridState.HOLD:
+            return "Hold";
+        case GridState.BOMB:
+            return "Dead";
+        default:
+            return "Open";
+    }
+}
+
+function getGridDisplay(gridState: BlockState[], index: number) {
+
+    if (gridState[index].state === GridState.BOMB)
+        return "💣";
+
+    if (gridState[index].state === GridState.OPENED)
+        return gridState[index].mineAroundCount === 0 ? "" : gridState[index].mineAroundCount;
+
+    if (gridState[index].state === GridState.HOLD)
+        return "🚩";
+
+    return "";
+}
+
+
+
+
+function createGameGrid(col: number, row: number, gridState: BlockState[], call: Function) {
+    const list: any[] = [];
+    for (let i: number = 0; i < row; ++i) {
+        for (let j: number = 0; j < col; ++j) {
+            const div = <div
+                className={getAppearClass(gridState[j + i * col])}
+                onMouseDown={call.bind(null, j + i * col)}
+                onMouseUp={call.bind(null, j + i * col)}
+                key={`${i}_${j}`}>
+                {getGridDisplay(gridState, j + i * col)}
+            </div>;
+            list.push(div);
+        }
+    }
+
+    return list;
+}
+
+
+function createGame(col: number, row: number, gridState: BlockState[], call: Function) {
+    return (
+        <div className='InnerFrame'>
+        <div className='Grid'>
+            {createGameGrid(col, row, gridState, call)}
+        </div>
+        </div>
+    )
+}
+
+
+
+export default function MainGame(props: {level: ILevel; game: Game; onMouseEvent: Function})
+{
+    return (
+        createGame(props.level.col, props.level.row, props.game.gridState, props.onMouseEvent
+            )
+    )
+}
--- src/components/mineswapper/index.tsx
+++ src/components/mineswapper/index.tsx
@@ -2,74 +2,13 @@
 
 import { useEffect, useState } from 'react';
 
-import {BlockState, Game,  levelSetting, GridState} from '../../api/Game'
+import { Game,  levelSetting, GridState} from '../../api/Game'
 
 import Countdown from "./Countdown"
 import ResetButton from "./ResetButton"
+import MainGame from "./MainGame"
 
 let clickCount = 0;
-
-
-function getAppearClass(state: BlockState) {
-    switch (state.state) {
-        case GridState.BLOCKED:
-            return "Block";
-        case GridState.OPENED:
-            return `Open Count${state.mineAroundCount}`;
-        case GridState.HOLD:
-            return "Hold";
-        case GridState.BOMB:
-            return "Dead";
-        default:
-            return "Open";
-    }
-}
-
-function getGridDisplay(gridState: BlockState[], index: number) {
-
-    if (gridState[index].state === GridState.BOMB)
-        return "💣";
-
-    if (gridState[index].state === GridState.OPENED)
-        return gridState[index].mineAroundCount === 0 ? "" : gridState[index].mineAroundCount;
-
-    if (gridState[index].state === GridState.HOLD)
-        return "🚩";
-
-    return "";
-}
-
-
-
-
-function createGameGrid(col: number, row: number, gridState: BlockState[], call: Function) {
-    const list: any[] = [];
-    for (let i: number = 0; i < row; ++i) {
-        for (let j: number = 0; j < col; ++j) {
-            const div = <div
-                className={getAppearClass(gridState[j + i * col])}
-                onMouseDown={call.bind(null, j + i * col)}
-                onMouseUp={call.bind(null, j + i * col)}
-                key={`${i}_${j}`}>
-                {getGridDisplay(gridState, j + i * col)}
-            </div>;
-            list.push(div);
-        }
-    }
-
-    return list;
-}
-
-
-function createGame(col: number, row: number, gridState: BlockState[], call: Function) {
-    return (
-        <div className='InnerFrame'>
-        <div className='Grid'>
-            {createGameGrid(col, row, gridState, call)}
-        </div>
-        </div>
-    )
-}
 
 function MineSwapper() {
 
@@ -98,9 +37,10 @@ function MineSwapper() {
                         gameState={game.gameState}></ResetButton>
                     <Countdown startTime={startTime}></Countdown>
                 </div >
-
-                {createGame(level.col, level.row, game.gridState,
-                    (i: number, ev: MouseEvent) => {
+                <MainGame 
+                    game={game}
+                    level={level}
+                    onMouseEvent={(i: number, ev: MouseEvent) => {
                         console.log(ev)
                         if (startTime === 0) {
                             setStartTime(Date.now());
@@ -142,7 +82,8 @@ function MineSwapper() {
                         setGame({ ...game });
                         console.log("Clicked" + (i));
                         console.log("Clicked" + game.gridState[i].state);
-                    })}
+                    }} />
+                
 
 
             </div>
